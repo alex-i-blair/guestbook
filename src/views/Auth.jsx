7b92b959adf7fda5e83dafd33904186dd5878018
Alex Blair <bclan11@gmail.com>
2022-05-05 16:16:39 -0700

    I had a bunch of commits but had to nuke my repo and start over. So far: env, services, App, context, finished with routes to basic views

--- src/views/Auth.jsx
+++ src/views/Auth.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
 import { useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import { useUser } from '../context/UserContext';
 
 export default function Auth() {
   const { login } = useUser();
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const location = useLocation();
   const history = useHistory();
@@ -17,8 +16,26 @@ export default function Auth() {
       await login(email, password);
       const url = location.state.origin ? location.state.origin.pathname : '/';
     } catch (error) {
-      
+      setError(error.message)
     }
   }
-  return <div>Auth</div> 
+  return (
+    <>
+    <h1>auth</h1>
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="email" 
+        value={email} 
+        onChange={(e) => setEmail(e.target.value)} 
+        placeholder="Email" />
+      <input 
+        type="password" 
+        value={password} 
+        onChange={(e) => setPassword(e.target.value)} 
+        placeholder="Password" />
+      <button type='submit'>Sign in</button>
+      <p>{error}</p>
+    </form>
+    </>
+    )
 } 
