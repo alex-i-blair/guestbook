9be1125acdf3b35fb4cf64723757c1a1576239f2
Alex Blair <bclan11@gmail.com>
2022-05-06 11:38:36 -0700

    logout button working and redirecting to auth

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,10 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useUser } from '../context/UserContext';
+import { getEntries } from '../services/entries';
 
 export default function Dashboard() {
   const { logout } = useUser();
   const [entries, setEntries] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  return <div>Dashboard</div>;
+  useEffect(() => {
+    //This is soooo much better/cleaner than async await imo
+    getEntries()
+      .then(setEntries)
+      .catch(console.error)
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <button onClick={logout}>logout</button>
+    </>
+  );
 }
